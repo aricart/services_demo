82eb6023ee17fce50306bae8489304773da9246e
aricart <alberto@synadia.com>
2022-11-04 15:51:03 -0400

    added a frequency service and cli

---
+++ frequency-service.ts
@@ -0,0 +1,56 @@
+import {
+  addService,
+  connect,
+  JSONCodec,
+} from "https://raw.githubusercontent.com/nats-io/nats.deno/dev/src/mod.ts";
+
+type Badge = {
+  name: string;
+  company?: string;
+};
+
+const jc = JSONCodec();
+const nc = await connect({ servers: "demo.nats.io" });
+
+const r: Record<string, number> = {};
+
+const sub = nc.subscribe("generate.badge");
+(async () => {
+  for await (const m of sub) {
+    const badge = jc.decode(m.data) as Badge;
+    const name = badge.name ?? "";
+    if (name === "") {
+      return;
+    }
+    const names = name.split(" ");
+    const n = names[0].toLowerCase();
+    r[n] = (r[n] || 0) + 1;
+  }
+})().catch();
+
+const service = await addService(nc, {
+  name: "frequency_service",
+  version: "0.0.1",
+  description: "nonitors names",
+  endpoint: {
+    subject: "badge.freq",
+    handler: (err, msg): Error | void => {
+      if (err) {
+        return err;
+      }
+      msg.respond(jc.encode(r));
+    },
+  },
+});
+
+service.done.then((err: Error | null) => {
+  if (err) {
+    console.log(`${service.name} stopped with error: ${err.message}`);
+  } else {
+    console.log(`${service.name} stopped.`);
+  }
+});
+
+console.log(
+  `${service.name} ${service.version} started with ID: ${service.id}`,
+);
---
+++ get-freq.ts
@@ -0,0 +1,52 @@
+import { cli, Command, Flags } from "https://deno.land/x/cobra/mod.ts";
+import {
+  connect,
+  Empty,
+  JSONCodec,
+  NatsConnection,
+} from "https://raw.githubusercontent.com/nats-io/nats.deno/dev/src/mod.ts";
+
+const root = cli({
+  use: "get-freq ",
+  run: async (cmd: Command, args: string[], flags: Flags): Promise<number> => {
+    const nc = await createConnection(flags);
+    const msg = await nc.request(
+      "badge.freq",
+      Empty,
+      { timeout: 30 * 1000 },
+    );
+    const jc = JSONCodec();
+    console.table(jc.decode(msg.data));
+    return 0;
+  },
+});
+root.addFlag({
+  short: "n",
+  name: "name",
+  type: "string",
+  usage: "your name",
+  default: "",
+  persistent: true,
+});
+root.addFlag({
+  name: "company",
+  type: "string",
+  usage: "your company",
+  default: "",
+  persistent: true,
+});
+
+root.addFlag({
+  name: "server",
+  type: "string",
+  usage: "NATS server to connect to",
+  default: "demo.nats.io",
+  persistent: true,
+});
+
+async function createConnection(flags: Flags): Promise<NatsConnection> {
+  const servers = [flags.value<string>("server")];
+  return connect({ servers });
+}
+
+Deno.exit(await root.execute(Deno.args));
